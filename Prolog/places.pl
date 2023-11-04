6fa15e26d0b17712a1185a2503386a99e9cb5e10
mpakulsk <01169219@pw.edu.pl>
2023-11-04 15:11:53 +0100

    add some places

---
+++ Prolog/places.pl
@@ -0,0 +1,9 @@
+place(vault, "place where diamond was stored but there is nothing anymore").
+place(hall, "big, crouded, splendid chamber with big throne in the center").
+place(kitchen, "cooking place with the dishes for the king").
+place(butler_room, "there are keys to every room on the castle").
+place(garden, "big garden, there are vegetables growing and is many flowers").
+place(guard_house, "small chamber with lots of weapons").
+place(wizard_house, "small, incredible place with many curious magic subjects").
+place(servants_house, "place with bedrooms for all workforces").
+place(forest, "big forest with wild animals and many plants and mushrooms"). 
